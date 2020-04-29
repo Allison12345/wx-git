2d77076559f3f23f9c9f706babb325e1cfe7123a
Allison <2562392373@qq.com>
2020-04-29 14:46:50 +0800

    my-picker

--- pages/trending/index.js
+++ pages/trending/index.js
@@ -25,6 +25,17 @@ Page({
     ],
     pickerIndex: [1, 1]
   },
+  onLoad(){
+    wx.request({
+      url: '',
+      header: {
+        'content-type': 'application/json'
+      },
+      success: res => {
+        console.log(res)
+      }
+    })
+  },
   onLanguageTap() {
     wx.navigateTo({ url: '/pages/languages/index' })
   },
