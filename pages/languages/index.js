e3e8c783cbefb6d60953c315fca797e676810a36
Allison <2562392373@qq.com>
2020-05-14 14:03:20 +0800

    language page

--- pages/languages/index.js
+++ pages/languages/index.js
@@ -1,27 +1,40 @@
 const laguagesItems = new Array(26).fill(0).map((_, index) => ({
   word: String.fromCodePoint(65 + index).toLowerCase(),
-  label: ['df', 'sf', 'sdqf', 'sdsdff', 'javaScript']
+  label: ['dfjdglfkajs', 'sfsd', 'sdqf', 'sdsdff', 'javaScript']
 }))
 Page({
   data: {
-    chooseItems:[],
+    chooseItems: [],
     laguagesItems,
-    wordItems: laguagesItems.map(language => language.word)
+    wordItems: laguagesItems.map(language => language.word),
+    activeWord: ''
   },
-  onChooseItem(items){
-    const {label,index} = items.detail
+  onChooseItem(items) {
+    const { label, index } = items.detail
     const chooseItems = this.data.chooseItems
-    if(chooseItems.indexOf(label) === -1){
+    if (chooseItems.indexOf(label) === -1) {
       chooseItems.push(label)
     }
-    this.setData({chooseItems})
+    this.setData({ chooseItems })
   },
-  onDeleteItem(index){
+  onDeleteItem(index) {
     const chooseItems = this.data.chooseItems
-    chooseItems.splice(index.detail,1)
-    this.setData({chooseItems})
+    chooseItems.splice(index.detail, 1)
+    this.setData({ chooseItems })
   },
-  moveToItem(item){
-    console.log(item.detail)
+  moveToItem(item) {
+    const word = item.detail
+    this.activeWord = word
+    wx.pageScrollTo({
+      scrollTop: 0,
+      duration: 300,
+      selector: `#${word}`,
+      success: res => {
+        console.log(res)
+      },
+      fail: res => {
+        console.log(res, fail)
+      }
+    })
   }
 })
